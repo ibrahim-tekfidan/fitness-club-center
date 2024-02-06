2c0359113ede1d478fe7fa9078b25b100518f41d
Ibrahim Tekfidan <ibrahim.tekfidn@gmail.com>
2024-02-06 23:41:57 +0300

    Build responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { Grid, GridItem } from '@chakra-ui/react';
 import React from 'react';
 
 const App = () => {
-  return <h1>Hello React</h1>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `'nav' 'main' 'footer'`,
+      }}
+      gridTemplateColumns={{
+        base: '1fr',
+      }}
+    >
+      <GridItem area={'nav'}>Nav</GridItem>
+      <GridItem area={'main'}>Main</GridItem>
+      <GridItem area={'footer'}>Footer</GridItem>
+    </Grid>
+  );
 };
 
 export default App;
