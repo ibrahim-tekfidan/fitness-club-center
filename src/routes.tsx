66d8b840d4d5a82d7eeca6ae5c5e08fddc218d0d
Ibrahim Tekfidan <ibrahim.tekfidn@gmail.com>
2024-02-09 09:16:17 +0300

    Handle errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Layout from './pages/Layout';
 import ExerciseDetailsPage from './pages/ExerciseDetailsPage';
+import ErrorPage from './pages/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       { path: 'exercises/:name', element: <ExerciseDetailsPage /> },
